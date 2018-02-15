81cedf65bd3425571653b05fe861e00d380d033e
Giovani <sousa_giovani@hotmail.com>
2018-02-15 17:54:18 -0200

    Add success to themes

--- youtube_channel/my_videos/models.py
+++ youtube_channel/my_videos/models.py
@@ -1,26 +1,37 @@
 from django.db import models
 from django.utils import timezone
 
+from datetime import timedelta
 
 class Theme(models.Model):
     name = models.CharField(max_length=50, unique=True)
+
+    def success(self):
+        a_year_ago = timezone.now() - timedelta(days=365)
+        return sum([video.score for video in self.videos.filter(date_uploaded__gte=a_year_ago)])
+
+    def srt(self):
+        return self.name
 
 
 class Video(models.Model):
     title = models.CharField(max_length=50, unique=True)
     date_uploaded = models.DateTimeField(blank=True)
     views = models.IntegerField(default=0)
-    themes = models.ManyToManyField(Theme)
+    themes = models.ManyToManyField(Theme, related_name='videos')
 
     @property
     def score(self):
         days_since_upload = (timezone.now() - self.date_uploaded).days
         time_factor = max(0, 1 - days_since_upload/365)
 
-        good_comments = self.comments.filter(is_positive=True).count()/self.comments.count()
-        thumbs_up = self.thumbs.filter(is_positive=True).count()/self.thumbs.count()
-        positivity_factor = 0.7 * good_comments + 0.3 * thumbs_up
+        all_comments = self.comments.count()
+        good_comments = self.comments.filter(is_positive=True).count()/all_comments if all_comments > 0 else 0
 
+        all_thumbs = self.thumbs.count()
+        thumbs_up = self.thumbs.filter(is_positive=True).count()/all_thumbs if all_thumbs > 0 else 0
+
+        positivity_factor = 0.7 * good_comments + 0.3 * thumbs_up
         return self.views * time_factor * positivity_factor
 
 
--- youtube_channel/my_videos/tests/test_models.py
+++ youtube_channel/my_videos/tests/test_models.py
@@ -7,10 +7,29 @@ from youtube_channel.my_videos.models import Theme, Video, Comment, Thumb
 
 class TestTheme(TestCase):
 
+    def setUp(self):
+        self.theme = Theme.objects.create(name='Machine Learning')
+
     def test_uniqueness(self):
-        Theme.objects.create(name='Machine Learning')
         with self.assertRaises(IntegrityError):
             Theme.objects.create(name='Machine Learning')
+
+    def test_success(self):
+        fake_data = {'date_uploaded': timezone.now(),
+                     'views': 10}
+
+        videos = [Video.objects.create(title='Machine Learning', **fake_data),
+                  Video.objects.create(title='NLP', **fake_data)]
+
+        for video in videos:
+            video.themes.add(self.theme)
+            fake_interaction = {'time': timezone.now(), 'video': video}
+            Comment.objects.create(is_positive=True, **fake_interaction)
+            Comment.objects.create(is_positive=False, **fake_interaction)
+            Thumb.objects.create(is_positive=True, **fake_interaction)
+            Thumb.objects.create(is_positive=False, **fake_interaction)
+
+        self.assertEqual(10, self.theme.success())
 
 
 class TestVideo(TestCase):
