f4976fb622afd7e607e0170d70165cc439b34d42
Giovani <sousa_giovani@hotmail.com>
2018-02-15 16:39:28 -0200

    Create Thumb and Comment models

--- youtube_channel/my_videos/models.py
+++ youtube_channel/my_videos/models.py
@@ -10,3 +10,16 @@ class Video(models.Model):
     date_uploaded = models.DateTimeField(blank=True)
     views = models.IntegerField(default=0)
     themes = models.ManyToManyField(Theme)
+
+
+class Interaction(models.Model):
+    is_positive = models.BooleanField()
+    time = models.DateTimeField(blank=True)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+
+
+class Thumb(Interaction):
+    pass
+
+class Comment(Interaction):
+    pass
