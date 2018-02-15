ede21f4813b79fff9e1b5c89897ade9e74a7fe16
Giovani <sousa_giovani@hotmail.com>
2018-02-15 17:07:48 -0200

    Add score to Video

--- youtube_channel/my_videos/models.py
+++ youtube_channel/my_videos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Theme(models.Model):
@@ -11,15 +12,28 @@ class Video(models.Model):
     views = models.IntegerField(default=0)
     themes = models.ManyToManyField(Theme)
 
+    @property
+    def score(self):
+        days_since_upload = (timezone.now() - self.date_uploaded).days
+        time_factor = max(0, 1 - days_since_upload/365)
+
+        good_comments = self.comments.filter(is_positive=True).count()/self.comments.count()
+        thumbs_up = self.thumbs.filter(is_positive=True).count()/self.thumbs.count()
+        positivity_factor = 0.7 * good_comments + 0.3 * thumbs_up
+
+        return self.views * time_factor * positivity_factor
+
 
 class Interaction(models.Model):
     is_positive = models.BooleanField()
     time = models.DateTimeField(blank=True)
-    video = models.ForeignKey(Video, on_delete=models.CASCADE)
 
 
 class Thumb(Interaction):
+    video = models.ForeignKey(Video, related_name='thumbs', on_delete=models.CASCADE)
     pass
 
+
 class Comment(Interaction):
+    video = models.ForeignKey(Video, related_name='comments', on_delete=models.CASCADE)
     pass
